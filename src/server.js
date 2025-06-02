12eaeabada0163691e5af07dbc0f308872848e50
semaali159 <semaali1599@gmail.com>
2025-06-02 16:11:04 -0700

    v2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const booksRouter = require('./routes/booksRoutes')
-const db = require('./db')
-const app = express()
-app.use(express.json())//to read the body of req(from json to javascript obj)
-app.use('/books',booksRouter)
-app.listen(3000,()=>console.log('listening in port 3000'))
+const express = require("express");
+const booksRouter = require("./routes/booksRoutes");
+const db = require("./db");
+const app = express();
+app.use(express.json()); //to read the body of req(from json to javascript obj)
+app.use("/books", booksRouter);
+
+app.listen(3000, () => console.log("listening in port 3000"));
